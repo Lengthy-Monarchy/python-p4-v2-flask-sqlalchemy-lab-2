512b5fabf5e9f093b22780d77babb623ec200539
Lengthy_monarchy <anneirungu07@gmail.com>
2024-04-07 08:54:39 +0300

    completeish

--- server/models.py
+++ server/models.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import relationship
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy_serializer import SerializerMixin
 
 
 metadata = MetaData(naming_convention={
@@ -16,6 +17,7 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    reviews = relationship('Review', back_populates='customer')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
@@ -27,6 +29,20 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
+    reviews = relationship('Review', back_populates='item')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+    
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, ForeignKey('items.id'))
+    customer = relationship('Customer', back_populates='reviews')
+    item = relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
